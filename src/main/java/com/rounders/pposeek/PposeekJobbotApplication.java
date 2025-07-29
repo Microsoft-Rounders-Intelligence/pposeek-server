cf5e9cd0588e9704a57469a2a231541ccef045bd
Sean <102026795+SiunKimm@users.noreply.github.com>
2025-07-29 13:15:03 +0900

    feat: 프로젝트 초안 업로드

---
+++ src/main/java/com/rounders/pposeek/PposeekJobbotApplication.java
@@ -0,0 +1,13 @@
+package com.rounders.pposeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PposeekJobbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PposeekJobbotApplication.class, args);
+	}
+
+}
